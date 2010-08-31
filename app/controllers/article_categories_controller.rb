602091be345d3a811a739990886561b3a442046e
jason gagne <jgagne33@gmail.com>
2010-08-31 13:57:39 -0700

    adding 404

--- app/controllers/article_categories_controller.rb
+++ app/controllers/article_categories_controller.rb
@@ -4,17 +4,14 @@ class ArticleCategoriesController < ApplicationController
   before_filter :add_crumbs
   before_filter :find_page
   before_filter :find_articles_for_sidebar
+  before_filter :get_article_category_or_404
 
   def show
     begin
-      @article_category = ArticleCategory.active.find(params[:id])
       @page = Page.find_by_permalink!('blog') if @article_category.menus.empty?
       @article_category.menus.empty? ? @menu = @page.menus.first : @menu = @article_category.menus.first
       @articles = @article_category.articles.published.paginate(:page => params[:page], :per_page => 10, :include => :article_categories)
       add_breadcrumb @article_category.name
-    rescue ActiveRecord::RecordNotFound
-      redirect_to '/404.html'
-    end
     respond_to do |wants|
       wants.html # index.html.erb
       wants.xml { render :xml => @articles.to_xml }
@@ -39,6 +36,8 @@ class ArticleCategoriesController < ApplicationController
     @article_authors = Person.active.find(:all, :conditions => "articles_count > 0")
     @article_tags = Article.published.tag_counts.sort_by(&:name)
   end
-
+  def get_article_category_or_404
+    render_404 unless @article_category = ArticleCategory.active.find(params[:id])
+  end
 end
 
