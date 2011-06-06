18f07c48e8839456fc8d039cfb6f94b7e65ec8cb
Ameravant <contact@ameravant.com>
2011-06-06 14:58:41 -0700

    modifying preview to submit html through ajax to cms.yml preview field

--- app/controllers/admin/articles_controller.rb
+++ app/controllers/admin/articles_controller.rb
@@ -97,12 +97,17 @@ class Admin::ArticlesController < AdminController
     @images = @article.images
     params[:article_article_category_id].blank? ? @side_column_sections = ColumnSection.all(:conditions => {:column_id => @page.column_id, :visible => true}) : @side_column_sections = ColumnSection.all(:conditions => {:column_id => ArticleCategory.find(params[:article_article_category_id]).column_id, :visible => true})
     @owner = @article
-    @article.body = params[:article_body]
+    #@article.body = params[:article_body]
     @article.person = Person.find(params[:article_person_id])
     @article.title = params[:article_title]
     @article.description = params[:article_description]
     @article.blurb = params[:article_blurb]
     @article.published_at = Time.now if params[:article_id].blank?
+  end
+  
+  def post_preview
+    @cms_config['site_settings']['preview'] = params[:post_preview][:body]
+    File.open("#{RAILS_ROOT}/config/cms.yml", 'w') { |f| YAML.dump(@cms_config, f) }
   end
   
   private
