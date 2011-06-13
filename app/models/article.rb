d20d03d233931c66b684a307f396e0ed2410f661
Ameravant <contact@ameravant.com>
2011-06-13 16:51:29 -0700

    subtracting 59 seconds from published at time

--- app/models/article.rb
+++ app/models/article.rb
@@ -11,7 +11,7 @@ class Article < ActiveRecord::Base
   has_many :assets, :as => :attachable, :dependent => :destroy
   validates_presence_of :title, :body
   accepts_nested_attributes_for :images
-  named_scope :published, { :conditions => ["published_at <= ? and published = ?", Time.now, true] }
+  named_scope :published, { :conditions => ["published_at <= ? and published = ?", (Time.now - 59.seconds), true] }
   named_scope :published_in_month, lambda { |month, year| {
     :conditions => { :published_at => Date.civil(year, month, 1).to_time..(Date.civil(year, month, 1) >> 1).to_time, :published => true }
     }
