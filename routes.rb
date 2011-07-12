60cf1890dd04767f516a141789a79f816685c9cd
Ameravant <contact@ameravant.com>
2011-07-12 14:00:24 -0700

    modifications to article_for_main_column

--- routes.rb
+++ routes.rb
@@ -4,7 +4,7 @@ resources :articles, :as => article_path, :member => { :comment => :post }, :has
 namespace :admin do |admin|
   admin.resources :article_categories, :has_many => { :features, :menus } do |article_category|
     article_category.resources :menus
-    article_category.resources :images, :member => { :reorder => :put }, :collection => { :reorder => :put }
+    article_category.resources :images, :member => { :reorder => :put }, :collection => { :reorder => :put, :add_multiple => :get }
   end
   admin.resources :articles, :has_many => [ :comments, :features, :assets ], :member => { :reorder => :put, :post_preview => :put }, :collection => { :preview => :get, :post_preview => :put } do |article|
     # member do
