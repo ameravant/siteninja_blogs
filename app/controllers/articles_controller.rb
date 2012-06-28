d52842b3d4732aa27d5b2861bfc77232ea21564a
Dave Myers <contact@ameravant.com>
2012-06-28 10:15:07 -0700

    fixing article.rss error

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -65,18 +65,16 @@ class ArticlesController < ApplicationController
   end
 
   def find_page
-    unless request.request_uri.include?('.rss')
-      @page = Page.find_by_permalink!('blog')
-      @tmplate = @page.template unless @page.template.blank?
-      @tmplate.layout_top = @global_template.layout_top if @tmplate.layout_top.blank?
-      @tmplate.layout_bottom = @global_template.layout_bottom if @tmplate.layout_bottom.blank?
-      @tmplate.article_show = @global_template.article_show if @tmplate.article_show.blank?
-      @tmplate.articles_index = @global_template.articles_index if @tmplate.articles_index.blank?
-      @tmplate.small_article_for_index = @global_template.small_article_for_index if @tmplate.small_article_for_index.blank?
-      @tmplate.medium_article_for_index = @global_template.medium_article_for_index if @tmplate.medium_article_for_index.blank?
-      @tmplate.large_article_for_index = @global_template.large_article_for_index if @tmplate.large_article_for_index.blank?
-      @menu = @page.menus.first
-    end
+    @page = Page.find_by_permalink!('blog')
+    @tmplate = @page.template unless @page.template.blank?
+    @tmplate.layout_top = @global_template.layout_top if @tmplate.layout_top.blank?
+    @tmplate.layout_bottom = @global_template.layout_bottom if @tmplate.layout_bottom.blank?
+    @tmplate.article_show = @global_template.article_show if @tmplate.article_show.blank?
+    @tmplate.articles_index = @global_template.articles_index if @tmplate.articles_index.blank?
+    @tmplate.small_article_for_index = @global_template.small_article_for_index if @tmplate.small_article_for_index.blank?
+    @tmplate.medium_article_for_index = @global_template.medium_article_for_index if @tmplate.medium_article_for_index.blank?
+    @tmplate.large_article_for_index = @global_template.large_article_for_index if @tmplate.large_article_for_index.blank?
+    @menu = @page.menus.first
   end
 
   def find_articles_for_sidebar
