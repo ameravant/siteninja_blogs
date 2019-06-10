23ad318893981c6659522a9e8386ba76b522017e
Dave Myers <dmyers722@gmail.com>
2019-06-10 10:27:00 -0700

    changing redirect path to form value

--- app/controllers/admin/articles_controller.rb
+++ app/controllers/admin/articles_controller.rb
@@ -92,7 +92,7 @@ class Admin::ArticlesController < AdminController
       flash[:notice] = "Article \"#{@article.title}\" updated."
       log_activity("Updated \"#{@article.title}\"")
       session[:cache] = true
-      redirect_to session[:redirect_path] ? session[:redirect_path] : admin_articles_path
+      redirect_to params[:redirect_path] ? params[:redirect_path] : admin_articles_path
     else
       render :action => "edit"
     end
